e8d8347b36c6fae0a3f4f8320e8bd06bc469134c
ameena28 <ameenakhatun821@gmail.com>
2020-06-18 17:28:36 -0400

    added slce.py

--- slice.py
+++ slice.py
@@ -1,3 +1,11 @@
 players=['elli','alice','arina','mice','fog']
 print(players[-3:])
 print(players[2:])
+
+#tuples
+print("buffet restaurant")
+recipes=('rice','dal','chapathi','mung dal','raitha')
+for recipe in recipes:
+	print(recipe)
+
+
